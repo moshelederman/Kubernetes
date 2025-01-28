f0fc253297575ebb298158e02daf0a312d2a5f09
moshelederman <mos9972@gmail.com>
2025-01-28 15:10:58 +0200

    a

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 provider "azurerm" {
   features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
+
 variable "client_id" {
   description = "Azure Client ID"
   sensitive   = true
