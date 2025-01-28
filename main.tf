9a5227baca4453c18f551c89123880d7320c04e5
moshelederman <mos9972@gmail.com>
2025-01-28 14:06:19 +0200

    added secrets

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
 terraform {
   backend "s3" {
     bucket         = "moshe-terrarorm"
